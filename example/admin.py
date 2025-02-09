0d0389dbbe5d169b504bc488cd260ede131dd45d
felipe huertas <fhuertas@unillanos.edu.co>
2025-02-09 17:27:02 -0500

    admin creado

--- example/admin.py
+++ example/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+# app/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+
+# Lista de modelos a registrar
+models = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]
+
+@admin.register(*models)
+class DayPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'morning', 'afternoon', 'cont', 'morning_marked', 'afternoon_marked', 'morning_authorized', 'afternoon_authorized')
+    list_filter = ('morning', 'afternoon', 'morning_authorized', 'afternoon_authorized')
+    search_fields = ('user__username',)
+    ordering = ('user',)
